06ead4461aeebcd2bc56207c658008f8f35bc858
helsonxiao <helsonxiao@gmail.com>
2022-04-17 22:12:59 +0800

    feat: compressed output

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
+echo 'building wasm...'
 GOOS=js GOARCH=wasm go build -o ./static/broccoli.wasm
+echo 'compressing wasm...'
+rm ./static/broccoli.br.wasm
+brotli -o ./static/broccoli.br.wasm ./static/broccoli.wasm
+echo 'done!'
