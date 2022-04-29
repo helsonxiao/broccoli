12c4518475ece3e2c518d70f20ba72660f71f8cd
helsonxiao <helsonxiao@gmail.com>
2022-04-29 23:49:04 +0800

    chore: separate compression from build

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 echo 'building wasm...'
 GOOS=js GOARCH=wasm go build -o ./static/broccoli.wasm
-echo 'compressing wasm...'
-rm ./static/broccoli.br.wasm
-brotli -o ./static/broccoli.br.wasm ./static/broccoli.wasm
 echo 'done!'
