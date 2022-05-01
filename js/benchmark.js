c9012732a2c52fd118fc13cd1608942528687531
helsonxiao <helsonxiao@gmail.com>
2022-05-01 16:45:01 +0800

    Updates

--- js/benchmark.js
+++ js/benchmark.js
@@ -2,6 +2,7 @@ import "../go/wasm_exec.js";
 // https://github.com/google/brotli/blob/master/js/decode.js
 import { BrotliDecode as jsBrotliDecode } from "./decode.js";
 // build manually using https://github.com/httptoolkit/brotli-wasm
+// https://github.com/helsonxiao/brotli-wasm/commit/e6e3bcc67ef855af0fa8e0b759699a3db38d83c8
 import initRust, * as rustBrotli from "../rust/brotli_wasm.js";
 
 const params = new URLSearchParams(location.search);
@@ -16,7 +17,8 @@ const textEncoder = new TextEncoder();
 const textDecoder = new TextDecoder();
 
 const msg =
-  '{"type":"message","event":"new","room_id":262451765,"user":{"user_id":9804407,"username":"迎宾机器猫小小川ゞ","iconurl":"http://static.missevan.com/avatars/202112/08/e40e541df9ca26088e9ddf26f0a30595234301.png","titles":[{"type":"level","level":49},{"type":"username","color":"#FF8686","colors":"#FF8686"},{"type":"noble","name":"新秀","level":2},{"type":"medal","name":"彼岸","super_fan":{"expire_time":1648915200},"level":18}]},"msg_id":"34ea0c75-a3dd-4149-8bf7-4d4f0caec46c","message":"@孙夫人s 来啦~ 小心炸弹！(╯‵□′)╯•••*～●，哈 骗你的啦~　"}'.repeat(messages);
+  '{"type":"message","event":"new","room_id":262451765,"user":{"user_id":9804407,"username":"迎宾机器猫小小川ゞ","iconurl":"http://static.missevan.com/avatars/202112/08/e40e541df9ca26088e9ddf26f0a30595234301.png","titles":[{"type":"level","level":49},{"type":"username","color":"#FF8686","colors":"#FF8686"},{"type":"noble","name":"新秀","level":2},{"type":"medal","name":"彼岸","super_fan":{"expire_time":1648915200},"level":18}]},"msg_id":"34ea0c75-a3dd-4149-8bf7-4d4f0caec46c","message":"@孙夫人s 来啦~ 小心炸弹！(╯‵□′)╯•••*～●，哈 骗你的啦~　"}'
+    .repeat(messages);
 
 if (language === "js" || language === "go") {
   if (!WebAssembly.instantiateStreaming) {
